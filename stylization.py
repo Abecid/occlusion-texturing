3e6963601e4b99bc4bc2343e79447f923ad33ece
Adam Lee <cmlee3211@gmail.com>
2024-08-23 18:54:44 +0900

    Add canny edge and add image path for controlnet

--- stylization.py
+++ stylization.py
@@ -6,16 +6,13 @@ from diffusers import StableDiffusionControlNetPipeline, ControlNetModel, UniPCM
 from PIL import Image
 import torch
 import yaml
+import cv2
 
 import config
 
-image_path = ""
-
 # Get depth estimation
-def depth_estimation(image_path):
+def depth_estimation(image):
     depth_estimator = pipeline('depth-estimation')
-
-    image = Image.open(image_path)
 
     image = depth_estimator(image)['depth']
     image = np.array(image)
@@ -25,8 +22,19 @@ def depth_estimation(image_path):
 
     return image
 
+def get_canny_edges(image):
+    low_threshold = 100
+    high_threshold = 200
+
+    image = cv2.Canny(image, low_threshold, high_threshold)
+    image = image[:, :, None]
+    image = np.concatenate([image, image, image], axis=2)
+    image = Image.fromarray(image)
+
+    return image
+
 # Generate image with controlnet
-def generate_texture(depth_image, text_condition, num_steps, depth_model="lllyasviel/sd-controlnet-depth", sd_model="runwayml/stable-diffusion-v1-5"):
+def generate_style(depth_image, text_condition, num_steps, depth_model="lllyasviel/sd-controlnet-depth", sd_model="runwayml/stable-diffusion-v1-5"):
     controlnet = ControlNetModel.from_pretrained(
         depth_model, torch_dtype=torch.float16
     )
@@ -37,30 +45,37 @@ def generate_texture(depth_image, text_condition, num_steps, depth_model="lllyas
 
     pipe.scheduler = UniPCMultistepScheduler.from_config(pipe.scheduler.config)
 
+    if torch.cuda.is_available():
+        pipe.to('cuda')
+
     pipe.enable_model_cpu_offload()
 
     image = pipe(text_condition, depth_image, num_inference_steps=num_steps).images[0]
 
     return image
 
-def apply_texture(texture_image):
+def apply_texture(stylized_image):
     pass
 
 def main(config_data):
     depth_model = config_data['depth_model']
     sd_model = config_data['stable_diffusion']['model']
     num_steps = config_data['stable_diffusion']['num_steps']
-    text_condition = config_data['text_condition']
+    text_condition = config_data['stylization']['text_condition']
+    image_path = config_data['stylization']['image_path']
     output_path = config_data['output']['texture']
     mesh_path = config_data['asset']['path']
+    mesh_name = os.path.splitext(os.path.basename(mesh_path))[0]
 
-    depth_image = depth_estimation(image_path)
-    texture_image = generate_texture(depth_image, text_condition, num_steps)
+    image = Image.open(image_path)
+
+    depth_image = depth_estimation(image)
+    stylized_image = generate_style(depth_image, text_condition, num_steps)
     
-    filename = "texture.png"
+    filename = f"stylized_{mesh_name}.png"
     output_path = os.path.join(output_path, filename)
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
-    texture_image.save(output_path)
+    stylized_image.save(output_path)
 
 if __name__ == "__main__":
     args = config.get_config()
